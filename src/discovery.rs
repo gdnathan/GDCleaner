ef22f54c6025c1f0d89600a38f31b7249ff59613
Nathan Gardet-Derc <nathan.gardet-derc@pm.me>
2026-04-21 20:35:17 +0200

    [ADD] not adding cleaned projects to total

--- src/discovery.rs
+++ src/discovery.rs
@@ -1,11 +1,11 @@
 use crate::settings::Config;
-use std::fs::read_dir;
-use std::path::PathBuf;
-use std::sync::{Arc, OnceLock};
-use tokio::sync::{Semaphore, OwnedSemaphorePermit};
-use tokio::task::JoinSet;
 use futures::future::{BoxFuture, FutureExt};
+use std::path::PathBuf;
 use std::process::Command;
+use std::sync::{Arc, OnceLock};
+use tokio::fs::read_dir;
+use tokio::sync::{OwnedSemaphorePermit, Semaphore};
+use tokio::task::JoinSet;
 
 static MAX_TASKS: OnceLock<Arc<Semaphore>> = OnceLock::new();
 static DU_FAILED: OnceLock<()> = OnceLock::new();
@@ -17,7 +17,7 @@ pub fn sem() -> Arc<Semaphore> {
 pub struct Folder {
     pub path: PathBuf,
     pub name: String,
-    pub size: u64
+    pub size: u64,
 }
 
 pub struct IdentifiedFolder {
@@ -27,44 +27,40 @@ pub struct IdentifiedFolder {
     pub size: u64,
 }
 
-pub async fn scan_folder(config: Arc<Config>, path: PathBuf, _permit: OwnedSemaphorePermit) -> Vec<IdentifiedFolder> {
+pub async fn scan_folder(
+    config: Arc<Config>,
+    path: PathBuf,
+    _permit: OwnedSemaphorePermit,
+) -> Vec<IdentifiedFolder> {
     // let permit = MAX_TASKS.acquire().await.unwrap();
     let mut language_name = None;
     // Used so for example, a "target" folder in a node project will not be selected for removal
     // (path, file_name)
     let mut potential_targets: Vec<Folder> = vec![];
+    let mut empty = true;
 
-    //TODO: change this to use tokio::fs::read_dir
-    let try_files = read_dir(&path);
-    match try_files {
-        Ok(files) => {
-            // TODO: with tokio's read_dir, this will be a poll and not an iterator. 
-            // while let Some(entry) = dir.next_entry().await 
-            files.for_each(|file| match file {
-                Ok(file) => {
-                    let file_name = file.file_name();
-                    // todo I'm just getting the folder size, not the content size
-                    let file_size = get_size(&config, &path);
-                    let file_name = file_name.to_string_lossy().to_string();
-                    if let Some(name) = config.lang_identifier.get(&file_name) {
-                        language_name = Some(name.clone());
-                    }
-                    if config.lang_target.get(&file_name).is_some() {
-                        potential_targets.push(Folder {
-                            path: file.path(),
-                            name: file_name.to_string(),
-                            size: file_size
-                        })
-                    }
+    match read_dir(&path).await {
+        Ok(mut dir) => {
+            while let Ok(Some(entry)) = dir.next_entry().await {
+                empty = false;
+                let file_name = entry.file_name().to_string_lossy().to_string();
+                let size = get_size(&config, &entry.path());
+
+                if let Some(name) = config.lang_identifier.get(&file_name) {
+                    language_name = Some(name.clone());
                 }
-                Err(e) => {
-                    eprintln!("ooooops something went wrong: {e}");
+                if config.lang_target.get(&file_name).is_some() {
+                    potential_targets.push(Folder {
+                        path: entry.path(),
+                        name: file_name.to_string(),
+                        size: size,
+                    })
                 }
-            });
+            }
         }
         Err(e) => {
             eprintln!("Could not open {:?}: {}", path.file_name(), e);
-            return vec!()
+            return vec![];
         }
     }
 
@@ -73,14 +69,25 @@ pub async fn scan_folder(config: Arc<Config>, path: PathBuf, _permit: OwnedSemap
         return go_deeper(config, path).await;
     }
 
+    if empty {
+        drop(_permit);
+        return vec![];
+    }
+
     potential_targets.retain(|folder| config.lang_target.get(&folder.name).is_some());
-    let size = potential_targets.iter().fold(0, |size, folder| size + folder.size);
-    vec!(IdentifiedFolder {
-        project_name: path.file_name().expect("should be valid").to_string_lossy().to_string(),
+    let size = potential_targets
+        .iter()
+        .fold(0, |size, folder| size + folder.size);
+    vec![IdentifiedFolder {
+        project_name: path
+            .file_name()
+            .expect("should be valid")
+            .to_string_lossy()
+            .to_string(),
         language: language_name.expect("checked"),
         folders: potential_targets,
         size,
-    })
+    }]
 }
 
 fn go_deeper(config: Arc<Config>, path: PathBuf) -> BoxFuture<'static, Vec<IdentifiedFolder>> {
@@ -99,18 +106,21 @@ fn go_deeper(config: Arc<Config>, path: PathBuf) -> BoxFuture<'static, Vec<Ident
         for sd in subdirs {
             let config_clone = config.clone();
             let new_permit = sem().acquire_owned().await.unwrap();
-            let _ = set.spawn({
-                scan_folder(config_clone, sd, new_permit)
-            });
+            let _ = set.spawn({ scan_folder(config_clone, sd, new_permit) });
         }
 
-        set.join_all().await.into_iter().flatten().collect::<Vec<_>>()
-    }.boxed()
+        set.join_all()
+            .await
+            .into_iter()
+            .flatten()
+            .collect::<Vec<_>>()
+    }
+    .boxed()
 }
 
 fn get_size(config: &Arc<Config>, path: &PathBuf) -> u64 {
     if config.skip_size || DU_FAILED.get().is_some() {
-        return 0
+        return 0;
     }
     Command::new("du")
         .arg("-sb")
